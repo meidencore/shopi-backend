0740d94550c1ec585724465feacb4d3abfb4aa57
meidencore <danieleverdale@gmail.com>
2024-10-03 19:56:05 -0400

    created users service and database entity with prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
+import { DatabaseService } from 'src/database/database.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserRequest) {
-    return 'user created';
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async createUser(
+    data: CreateUserRequest,
+  ): Promise<{ id: number; email: string }> {
+    try {
+      return await this.databaseService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new UnprocessableEntityException('Email already exists.');
+      throw error;
+    }
   }
 }
