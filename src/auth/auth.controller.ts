2ad9c4b94443e2de94153297d301a67f412e37e0
meidencore <danieleverdale@gmail.com>
2024-10-06 10:35:17 -0400

    added the jwt-strategy for authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from '@prisma/client';
 import { Response } from 'express';
+import { JwtAuthGuard } from './guards/jwt-auth.guards';
+import { TokenPayload } from './interfaces/token-payload.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +18,11 @@ export class AuthController {
     @Res({ passthrough: true }) response: Response,
   ) {
     return await this.authService.login(user, response);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@CurrentUser() user: TokenPayload) {
+    return user;
   }
 }
