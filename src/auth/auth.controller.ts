18e0516475f7b7fbc83f4cb661b352124ba01712
meidencore <danieleverdale@gmail.com>
2024-10-06 16:42:41 -0400

    moved the endpoint auth/me to users/me

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,9 @@
-import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from '@prisma/client';
 import { Response } from 'express';
-import { JwtAuthGuard } from './guards/jwt-auth.guards';
-import { TokenPayload } from './interfaces/token-payload.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -18,11 +16,5 @@ export class AuthController {
     @Res({ passthrough: true }) response: Response,
   ) {
     return await this.authService.login(user, response);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('me')
-  async getMe(@CurrentUser() user: TokenPayload) {
-    return user;
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
 import { UsersService } from './users.service';
 import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guards';
+import { TokenPayload } from 'src/auth/interfaces/token-payload.interface';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -11,5 +21,10 @@ export class UsersController {
   @Post()
   createUser(@Body() request: CreateUserRequest) {
     return this.usersService.createUser(request);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@CurrentUser() user: TokenPayload) {
+    return user;
   }
 }
