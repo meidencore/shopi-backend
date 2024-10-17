ed622706555c622f028367167834358c8308c431
meidencore <danieleverdale@gmail.com>
2024-10-17 19:06:32 -0400

    upload image and serve image as static file done

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,24 +1,86 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guards';
 import { CreateProductRequest } from './dto/create-product.request';
 import { ProductsService } from './products.service';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { TokenPayload } from '../auth/interfaces/token-payload.interface';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { promises as fs } from 'fs';
 
+@UseGuards(JwtAuthGuard)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
   @Post()
-  @UseGuards(JwtAuthGuard)
   async createProducts(
     @Body() body: CreateProductRequest,
     @CurrentUser() user: TokenPayload,
   ) {
     return await this.productsService.createProduct(body, user.UserId);
   }
+
+  @Post(':productId/image')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/products',
+        filename: (req, file, callback) => {
+          callback(
+            null,
+            `${req.params.productId}${extname(file.originalname)}`,
+          );
+        },
+      }),
+    }),
+  )
+  async uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 5000000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return { message: `${file.originalname} uploaded correct` };
+  }
+
   @Get()
-  @UseGuards(JwtAuthGuard)
   async getProducts() {
-    return await this.productsService.getProducts();
+    const products = await this.productsService.getProducts();
+    return Promise.all(
+      products.map(async (product) => ({
+        ...product,
+        imageExists: await this.imageExists(product.id),
+      })),
+    );
+  }
+
+  private async imageExists(productId: number) {
+    try {
+      await fs.access(
+        join(__dirname, '../../', `public/products/${productId}.jpg`),
+        fs.constants.F_OK,
+      );
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
